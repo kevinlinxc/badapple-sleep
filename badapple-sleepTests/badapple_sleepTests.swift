28e4ca7c1c761bbd9ce86f22dfa3b077cb7f01e1
Kevin Lin <kevinlinxc@gmail.com>
2026-07-24 18:00:35 -0700

    Initial Commit

---
+++ badapple-sleepTests/badapple_sleepTests.swift
@@ -0,0 +1,17 @@
+//
+//  badapple_sleepTests.swift
+//  badapple-sleepTests
+//
+//  Created by Kevin Lin on 2026-07-24.
+//
+
+import Testing
+@testable import badapple_sleep
+
+struct badapple_sleepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
